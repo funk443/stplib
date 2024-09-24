b9598d0089158b0eee55e8d363e65afcaea1a886
CToID <funk443@yahoo.com.tw>
2024-09-25 02:47:35 +0800

    Implemented almost all definitions from ISO 10303-21 section 5.
    The `ANCHOR_NAME`, `UNIVERSAL_RESOURCE_IDENTIFIER`,
    `URI_FRAGMENT_IDENTIFIER`, and `SIGNATURE_CONTENT` is not yet
    implemented.

---
+++ parser.py
@@ -0,0 +1,172 @@
+from lark import Lark
+
+
+
+# ---- FIX ME! ----
+# URI, RESOURCE, ANCHOR_NAME, and SIGNATURE_CONTENT
+# not implemented.
+grammar = r"""
+%import common.WS
+%ignore WS
+
+SPACE: " "
+DIGIT: "0" .. "9"
+UPPER: "A" .. "Z" | "_"
+LOWER: "a" .. "z"
+SPECIAL: "!"
+    | "*"
+    | "$"
+    | "%"
+    | "&"
+    | "."
+    | "#"
+    | "+"
+    | ","
+    | "-"
+    | "("
+    | ")"
+    | "?"
+    | "/"
+    | ":"
+    | ";"
+    | "<"
+    | "="
+    | ">"
+    | "@"
+    | "["
+    | "]"
+    | "{"
+    | "|"
+    | "}"
+    | "^"
+    | "`"
+    | "~"
+    | "\""
+REVERSE_SOLIDUS: "\\"
+APOSTROPHE: "'"
+LATIN_CODE_POINT: SPACE
+    | DIGIT
+    | LOWER
+    | UPPER
+    | SPECIAL
+    | REVERSE_SOLIDUS
+    | APOSTROPHE
+
+keyword: USER_DIFINED_KEYWORD | STANDARD_KEYWORD
+USER_DIFINED_KEYWORD: "!" UPPER (UPPER | DIGIT)*
+STANDARD_KEYWORD: UPPER (UPPER | DIGIT)*
+
+SIGN: "+" | "-"
+INNER_INTEGER: SIGN? DIGIT+
+integer: INNER_INTEGER
+INNER_REAL: SIGN? DIGIT+ "." DIGIT* ("E" SIGN? DIGIT+)?
+real: INNER_REAL
+
+INNER_STRING: (SPECIAL
+     | DIGIT
+     | SPACE
+     | LOWER
+     | UPPER
+     | APOSTROPHE APOSTROPHE
+     | REVERSE_SOLIDUS REVERSE_SOLIDUS)+
+string: "'" INNER_STRING? "'"
+
+INNER_INSTANCE_NAME: DIGIT+
+entity_instance_name: "#" INNER_INSTANCE_NAME
+value_instance_name: "@" INNER_INSTANCE_NAME
+INNER_CONSTANT_NAME:  UPPER (UPPER | DIGIT)*
+constant_entity_name: "#" INNER_CONSTANT_NAME
+constant_value_name: "@" INNER_CONSTANT_NAME
+
+lhs_occurance_name: entity_instance_name
+    | value_instance_name
+rhs_occurance_name: entity_instance_name
+    | value_instance_name
+    | constant_entity_name
+    | constant_value_name
+
+INNER_TAG_NAME: (UPPER | LOWER) (UPPER | LOWER | DIGIT)*
+tag_name: INNER_TAG_NAME
+
+INNER_ENUMERATION: UPPER (UPPER | DIGIT)*
+enumeration: "." INNER_ENUMERATION "."
+
+universal_fragment_identifier: "#" (LOWER | UPPER | DIGIT | SPECIAL)+
+universal_resource_identifier: /https/
+resource: "<" universal_resource_identifier ">"
+
+signature_content: "FIXME!!"
+
+HEX: "0" .. "9" | "A" .. "F"
+binary: "\"" ("0" .. "3") HEX* "\""
+
+exchange_file: "ISO-10303-21;" \
+    header_section \
+    anchor_section? \
+    reference_section? \
+    data_section* \
+    "END-ISO-10303-21;" \
+    signature_section*
+
+header_section: "HEADER;" \
+    header_entity header_entity header_entity \
+    header_entity_list? \
+    "ENDSEC;"
+header_entity_list: header_entity+
+header_entity: keyword "(" parameter_list? ")" ";"
+
+parameter_list: parameter ("," parameter)*
+parameter: typed_parameter
+    | untyped_parameter
+    | omitted_parameter
+typed_parameter: keyword "(" parameter ")"
+untyped_parameter: "$"
+    | integer
+    | real
+    | string
+    | rhs_occurance_name
+    | enumeration
+    | binary
+    | list
+omitted_parameter: "*"
+
+list: "(" parameter_list? ")"
+
+anchor_section: "ANCHOR;" anchor_list "ENDSEC;"
+anchor_list: anchor*
+anchor: anchor_name "=" anchor_item anchor_tag* ";"
+anchor_name: "<" universal_fragment_identifier ">"
+anchor_item: "$"
+    | integer
+    | real
+    | string
+    | enumeration
+    | binary
+    | rhs_occurance_name
+    | resource
+    | anchor_item_list
+anchor_item_list: "(" (anchor_item ("," anchor_item)*)? ")"
+anchor_tag: "{" tag_name ":" anchor_item "}"
+
+reference_section: "REFERENCE;" reference_list "ENDSEC;"
+reference_list: reference*
+reference: lhs_occurance_name "=" resource ";"
+
+data_section: "DATA" ("(" parameter_list ")")? ";" \
+    entity_instance_list \
+    "ENDSEC;"
+entity_instance_list: entity_instance*
+entity_instance: simple_entity_instance
+    | complex_entity_instance
+simple_entity_instance: entity_instance_name "=" simple_record ";"
+complex_entity_instance: entity_instance_name "=" subsuper_record ";"
+simple_record: keyword "(" parameter_list? ")"
+subsuper_record: "(" simple_record_list ")"
+simple_record_list: simple_record+
+
+signature_section: "SIGNATURE" signature_content "ENDSEC;"
+"""
+
+
+
+l = Lark(grammar, start = "exchange_file")
